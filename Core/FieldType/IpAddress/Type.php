5a4e1ba5e7fed782c8d24ac034a490516d78c81c
Edi Modrić <edi.modric@gmail.com>
2013-08-28 13:29:38 +0200

    Step 5

--- Core/FieldType/IpAddress/Type.php
+++ Core/FieldType/IpAddress/Type.php
@@ -170,4 +170,25 @@ class Type extends FieldType
             throw new InvalidArgumentType( "$value", "\\Netgen\\EzscIpAddressBundle\\Core\\FieldType\\IpAddress\\Value", $value );
         }
     }
+
+    /**
+     * Returns information for FieldValue->$sortKey relevant to the field type.
+     *
+     * Return value is mixed. It should be something which is sensible for
+     * sorting.
+     *
+     * It is up to the persistence implementation to handle those values.
+     * Common string and integer values are safe.
+     *
+     * For the legacy storage it is up to the field converters to set this
+     * value in either sort_key_string or sort_key_int.
+     *
+     * @param \eZ\Publish\Core\FieldType\Value|\Netgen\EzscIpAddressBundle\Core\FieldType\IpAddress\Value $value
+     *
+     * @return mixed
+     */
+    protected function getSortInfo( BaseValue $value )
+    {
+        return $value->ipAddress;
+    }
 }
