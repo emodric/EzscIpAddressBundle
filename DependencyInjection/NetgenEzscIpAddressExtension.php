dae0ccaaf1f915c01f277bda696fe0cbeb51718f
Edi Modrić <edi.modric@gmail.com>
2013-08-28 12:18:17 +0200

    Step 4

--- DependencyInjection/NetgenEzscIpAddressExtension.php
+++ DependencyInjection/NetgenEzscIpAddressExtension.php
@@ -24,5 +24,6 @@ class NetgenEzscIpAddressExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('fieldtypes.yml');
+        $loader->load('storage_engines.yml');
     }
 }
